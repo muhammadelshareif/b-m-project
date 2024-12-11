7fbccc852f8359e9490ca88126a49a25c25d6a7f
muhammadelshareif <melshareif1@gmail.com>
2024-12-11 16:17:48 -0500

    Phase1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,6 +17,11 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
-  // ...
+  // backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
 
 module.exports = router;
